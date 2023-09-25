6c44c299299c7b91422d9d7ecbc0cb78b92f5b4a
Вікторія Кангер <viktoriakanger@MacBook-Pro-Viktoria.local>
2023-09-25 23:07:31 +0300

    commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age ;
+    int weight;
+    std::cout << "type tour age: ";
+    std::cin >> age;
+    std::cout << "type your weight";
+    std::cin >> weight;
+    std::cout << "Age: " << age << ", Weight: " << weight << std::endl;
+}
